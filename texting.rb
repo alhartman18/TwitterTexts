6477483e3bb9be0e37746a085192312e4fefa8d9
alhartman18 <alhartman18@gmail.com>
2014-06-24 11:00:56 -0400

    even more

--- texting.rb
+++ texting.rb
@@ -2,18 +2,18 @@ require 'twilio-ruby'
 
 class Texting
 
-    def send_text
+    def send_text(sender, recipient, body)
         account_sid = 'ACe330ba04d082392df4cb3511dcb72cec'
         auth_token = '2008ea097713e401a16c54029058da82'
 
         @client = Twilio::REST::Client.new(account_sid, auth_token)
         @client.account.messages.create(
-          :from => '+18152642023',
-          :to => '+19178036744',
-          :body => 'Hey Vorm!'
+          :from => sender,
+          :to => recipient,
+          :body => body
         )
     end
 end
 
 test = Texting.new
-test.send_text
+test.send_text(+18152642023, +19178036744, "Hey Vorm!")
