5654644b1130cdd363c2f4d8670b272c3b82aa2f
alhartman18 <alhartman18@gmail.com>
2014-06-24 10:43:56 -0400

    more

--- texting.rb
+++ texting.rb
@@ -1,14 +1,19 @@
 require 'twilio-ruby'
 
 class Texting
-account_sid = 'ACe330ba04d082392df4cb3511dcb72cec'
-auth_token = '2008ea097713e401a16c54029058da82'
-@client = Twilio::REST::Client.new(account_sid, auth_token)
-@client.account.messages.create(
-  :from => '+18152642023',
-  :to => '+19178036744',
-  :body => 'Hey Vorm!'
-)
+
+    def send_text
+        account_sid = 'ACe330ba04d082392df4cb3511dcb72cec'
+        auth_token = '2008ea097713e401a16c54029058da82'
+
+        @client = Twilio::REST::Client.new(account_sid, auth_token)
+        @client.account.messages.create(
+          :from => '+18152642023',
+          :to => '+19178036744',
+          :body => 'Hey Vorm!'
+        )
+    end
 end
 
 test = Texting.new
+test.send_text
